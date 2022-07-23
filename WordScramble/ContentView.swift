c82910115495c0b8680386b743235f8a10a96642
Burak Cüce <b.cuece@web.de>
2022-07-23 09:56:07 +0200

    - add start.txt - edit ContentView

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(useWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
     }
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt")
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            useWords.insert(answer, at: 0)
+            newWord = ""
+        }
     }
 }
 
